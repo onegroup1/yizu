fc0071593a8631dd928eaa0402df81e8e8d790e9
xvlirong <717814532@qq.com>
2016-06-14 14:10:03 +0800

    Update IndexController.class.php

--- one/admin/Home/Controller/IndexController.class.php
+++ one/admin/Home/Controller/IndexController.class.php
@@ -3,10 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $obj=M('index');
-        $data=$obj->sel(1);
-        print_r($data);
-            //$this->display('login');
+            $this->display('login');
     }
     public function login(){
         //echo 123;
