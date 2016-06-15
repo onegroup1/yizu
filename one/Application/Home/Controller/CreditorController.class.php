eb4f1475e912eeeab3b315129d7c6989dda5e770
hqj123 <1196069929@qq.com>
2016-06-15 21:35:41 +0800

    hqj 6.15

--- one/Application/Home/Controller/CreditorController.class.php
+++ one/Application/Home/Controller/CreditorController.class.php
@@ -2,11 +2,27 @@
 namespace Home\Controller;
 use Think\Controller;
 class CreditorController extends Controller {
-    //显示债权前台页面
-    public function zhaiw(){
+    //显示散标债权前台页面
+    public function zhais(){
         $User = M('Creditor');
-        $arr = $User->select();
+        $count= $User->count();// 查询满足要求的总记录数
+        $Page= new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数(10)
+        $Page -> setConfig('header','共%TOTAL_ROW%条');
+        $Page -> setConfig('first','首页');
+        $Page -> setConfig('last','共%TOTAL_PAGE%页');
+        $Page -> setConfig('prev','上一页');
+        $Page -> setConfig('next','下一页');
+        $Page -> setConfig('link','indexpagenumb');//pagenumb 会替换成页码
+        $Page -> setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        $show= $Page->show();// 分页显示输出
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $arr = $User->order('creditor_id')->limit($Page->firstRow.','.$Page->listRows)->select();
+        //$arr = $User->select();
+        for ($i=0;$i<count($arr);$i++) {
+            $arr[$i]['cred']=intval($arr[$i]['yet_bid']/$arr[$i]['creditor_money']*100);
+        }
         $this->assign("data", $arr);
+        $this->assign('page',$show);// 赋值分页输出
         $this->display();
     }
     //显示债权转让页面
@@ -16,8 +32,10 @@ class CreditorController extends Controller {
         $this->assign("data", $arr);
         $this->display();
     }
-    //显示债权登陆后页面
-    public function zhaiy(){
+
+    //显示
+    public function zhaizijin(){
         $this->display();
     }
+
 }
