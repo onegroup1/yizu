6e4a5988fb423ec4da2f5fe71e2448eaa87c5e2e
hqj123 <1196069929@qq.com>
2016-06-23 11:08:04 +0800

    hqj

--- one/Application/Home/Controller/CreditorController.class.php
+++ one/Application/Home/Controller/CreditorController.class.php
@@ -58,13 +58,23 @@ class CreditorController extends Controller {
         $this->display();
     }
 
-    //显示
+    //显示-------------------
     public function zhaiqxq(){
+        $sess=session('login_name');
+        //print_r($sess);die;
+        $sessid=$sess['login_id'];
+        if(!empty($sessid)){
+            $Login=M('Login');
+            $bala=$Login->where("login_id=$sessid")->select();
+            $balance1=$bala[0]['login_balance'];
+        }
+
+        //print_r($balance);die;
         $User=M('Bid');
         $id=$_GET['id'];
+        //print_r($id);die;
         $jindu=$_GET['jindu'];
-        //echo $jindu;die;
-        $sess=$_SESSION['users'];
+        //print_r($sessid);die;
         $arr2=$User->where("creditor_id=$id")->select();
         //计算投资的中金额
         $money1=0;
@@ -122,7 +132,7 @@ class CreditorController extends Controller {
             $this->assign("credlilv", $credlilv);
             $this->assign("money1", $money1);
             $this->assign("lilv", $lilv);
-            $this->assign("deadline", $deadline);
+            $this->assign("deadline1", $deadline1);
             $this->display('zqxq');
         }
         //登陆且不等于100%
@@ -132,12 +142,11 @@ class CreditorController extends Controller {
             $arr=$User
                 ->join("yi_login on yi_creditor.login_id=yi_login.login_id")
                 ->where("creditor_id=$id")->select();
-            //print_r($arr);die;
+
             for ($i=0;$i<count($arr);$i++) {
                 $money=$arr[$i]['creditor_money'];
                 $lilv=$arr[$i]['creditor_lilv'];
                 $deadline=$arr[$i]['creditor_deadline'];
-                $balance=$arr[$i]['login_balance'];
                 $title=$arr[$i]['creditor_name'];
             }
 
@@ -149,7 +158,7 @@ class CreditorController extends Controller {
             }
             $moneysub=str_replace(',','',$money);
             $moneys=$moneysub-$money1;
-            //print_r($arr);die;
+            //print_r($balance);die;
             $this->assign("data",$arr);
             $this->assign("title", $title);
             $this->assign("money", $money);
@@ -157,7 +166,7 @@ class CreditorController extends Controller {
             $this->assign("lilv", $lilv);
             $this->assign("jindu", $arr10);
             $this->assign("deadline", $deadline);
-            $this->assign("balance", $balance);
+            $this->assign("balance1", $balance1);
             $this->display('zqxqtb');
 
         }
@@ -175,13 +184,26 @@ class CreditorController extends Controller {
                 $deadline=$arr[$i]['creditor_deadline'];
                 $title=$arr[$i]['creditor_name'];
             }
+
+
+            $Userb=M('Bid');
+            $arr2=$Userb->where("creditor_id=$id")->select();
+            $money1=0;
+            for ($i=0;$i<count($arr2);$i++) {
+                $money1=$arr2[$i]['bid_money']+$money1;
+            }
+            $moneysub=str_replace(',','',$money);
+            $moneys=$moneysub-$money1;
+
            // print_r($arr10);die;
             $this->assign("money", $money);
             $this->assign("title", $title);
             $this->assign("money1", $money1);
+            $this->assign("moneys", $moneys);
             $this->assign("lilv", $lilv);
             $this->assign("jindu", $arr10);
             $this->assign("deadline", $deadline);
+            $this->assign("balance1", $balance1);
             $this->display('zqxqd');
 
         }
@@ -206,14 +228,21 @@ class CreditorController extends Controller {
             $this->assign("lilv", $lilv);
             $this->assign("jindu", $arr10);
             $this->assign("deadline", $deadline);
+            $this->assign("balance1", $balance1);
             $this->display('zqxqdl');
         }
     }
     //显示投标页面
     public function zqxqtb(){
+        $sess=session('login_name');
+        $sessid=$sess['login_id'];
+        $Login=M('Login');
+        $bala=$Login->where("login_id=$sessid")->select();
+        $balance1=$bala[0]['login_balance'];
+
+
         $id=$_GET['login_id'];
         $User=M('Login');
-
         //计算进度
         $Userc = M('Creditor');
         $rows = $Userc->select();
@@ -273,11 +302,22 @@ class CreditorController extends Controller {
         $this->assign("deadline", $deadline);
         $this->assign("creditor_id", $creditor_id);
         $this->assign("login_id", $_SESSION['login_name']['login_id']);
-        $this->assign("balance", $balance);
+        $this->assign("balance1", $balance1);
         $this->display();
     }
 
     public function bid_money(){
+        $sess=session('login_name');
+        $sessid=$sess['login_id'];
+        //print_r($sessid);die;
+        if(empty($sessid)){
+            $this->redirect('Logins/index');
+        }else{
+
+        $Login=M('Login');
+        $bala=$Login->where("login_id=$sessid")->select();
+        $balance1=$bala[0]['login_balance'];
+
         $User=M('Bid');
         $data['creditor_id']=$_GET['creditor_id'];
         $data['login_id']=$_GET['login_id'];
@@ -285,10 +325,11 @@ class CreditorController extends Controller {
         //print_r($data);die;
         if($User->add($data)){
             $Userlo=M('Login');
-            $data2 = $Userlo->where("login_id=".$data['login_id'])->find();
+            $data2 = $Userlo->where("login_id=".$sessid)->find();
+            //print_r($data2);die;
             $data1=$data2['login_balance']-$data['bid_money'];
             //print_r($data1);die;
-            $Userlo-> where("login_id=".$data['login_id'])->setField('login_balance',"$data1");
+            $Userlo-> where("login_id=".$sessid)->setField('login_balance',"$data1");
             $this->redirect('Creditor/zqxqtb',array(
                 'creditor_id' => $_GET['creditor_id'],
                 'login_id' => $_GET['login_id']
@@ -296,7 +337,52 @@ class CreditorController extends Controller {
         }else{
             $this->error('投标失败');
         }
+        }
     }
+
+    public function zqadd(){
+        $this->display();
+    }
+    public function add(){
+        $sess=session('login_name');
+        //print_r($sess);die;
+        $sessid=$sess['login_id'];
+        $User = M('Creditor');
+        $date['login_id']=$sessid[0]['login_id'];
+        $date['creditor_name']=$_POST['creditor_name'];
+        $date['creditor_lilv']=number_format($_POST['creditor_lilv'],2);
+        $date['creditor_money']=number_format($_POST['creditor_money'],2);
+        $date['creditor_deadline']=$_POST['creditor_deadline'];
+        $date['creditor_time']=time();
+        //print_r($date);die;
+        if($User->add($date)){
+            $this->redirect('Creditor/zhais');
+        }else{
+            $this->error();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //显示债权转让页面
     public function zhaiz(){
